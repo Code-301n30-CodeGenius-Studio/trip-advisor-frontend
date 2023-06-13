5d65b7daf0b1f18a6e03b8000e714ba270de8db9
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-06-12 21:27:54 -0700

    added styling

--- src/BestTrip.js
+++ src/BestTrip.js
@@ -1,18 +1,43 @@
 import React from "react";
-import "./App.css"
-import logo from "./img/CodeGenius-Studios-logos.jpeg"
-class BestTrip extends React.Component{
-  render(){
-    return(
-      <>
-    <h1>
-      <img className="logo" src={logo} alt="Logo"/>
-    </h1>
+import "./App.css";
+import {Card, Col} from "react-bootstrap"
+import background from "./img/yosemite.jpg";
+import kalisa from "./img/kalisa.jpg"
 
-   <p>Welcome to trip advisor website!</p>
-    </>
-    )
+class BestTrip extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 className="home_h1">Welcome Travelers!</h1>
+
+        <Col>
+        <div id="cardOne">
+        <Card  style={{ width: "400px", height:"600px", borderRadius: "30%" }}>
+          <Card.Img className="image" variant="top" src={background} />
+          <Card.Body>
+            <Card.Title className="title">Let's make traveling easy</Card.Title>
+            <Card.Text>
+              
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </div>
+        <div id="cardTwo">
+        <Card  style={{ width: "400px", height:"600px", borderRadius: "30%" }}>
+          <Card.Img className="image" variant="top" src={kalisa} />
+          <Card.Body>
+            <Card.Title className="title">Inspiring and mesmerizing</Card.Title>
+            <Card.Text>
+              
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </div>
+        </Col>
+       
+      </>
+    );
   }
 }
 
-export default BestTrip; 
+export default BestTrip;
