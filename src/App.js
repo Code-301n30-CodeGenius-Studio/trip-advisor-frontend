ccb6f482b254beda486eab73cb49fabbcb88d212
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-06-13 21:44:58 -0700

    auth

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React from 'react'
-import './App.css';
-import AboutUs from './About';
-import Header from './Header';
-import NationalPark from './NationalPark';
-import BestTrip from './BestTrip';
-import Footer from './Footer';
-import Main from './Main';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import AboutUs from "./About";
+import Header from "./Header";
+// import NationalPark from './NationalPark';
+import BestTrip from "./BestTrip";
+import Footer from "./Footer";
+import Main from "./Main";
+import { withAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-    <Router>
-      <Header />
-      <Routes>
-        
-        <Route 
-          exact path="/"
-          element={<BestTrip/>}></Route>       
+class App extends React.Component {
+  render() {
+    const { isAuthenticated } = this.props.auth0;
 
-        <Route
-        path="Advisor"
-        element={<Main/>}>
+    return (
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<BestTrip />}></Route>
 
-        </Route>
-        <Route
-          path="About"
-          element={<AboutUs/>}>
-        </Route>
-      </Routes>
-      <Footer />
-    </Router>
-  </>
-  );
+            <Route
+              path="Advisor"
+              // element={<Main />}
+              element={isAuthenticated ? <Main /> : <BestTrip />}
+            ></Route>
+
+            <Route path="About" element={<AboutUs />}></Route>
+          </Routes>
+          <Footer />
+        </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuth0(App);
---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css'
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
+import {withAuth0} from '@auth0/auth0-react';
+
+function Login() {
+
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+  } = useAuth0();
+
+  function handleLogin() {
+    loginWithRedirect();
+  }
+
+  return ! isAuthenticated &&
+    <Button className ='login' onClick={handleLogin}>Log in</Button>
+  ;
+}
+export default withAuth0(Login);
