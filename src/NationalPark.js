31f29c5b3565c6b6c0b6335efbc3b0a88859eca2
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-06-15 23:55:21 -0700

    dont merge savind 2

--- src/NationalPark.js
+++ src/NationalPark.js
@@ -11,7 +11,7 @@ class NationalPark extends React.Component {
     super(props);
     this.state = {
       isParkAdded: -1,
-      parks:{}
+      parks: {},
     };
   }
   renderYelpData(yelpData) {
@@ -37,14 +37,14 @@ class NationalPark extends React.Component {
     this.setState({ isParkAdded: idx });
   };
 
-  handleDelete = (park) => {
-    // const deleteUser = {
-    //   parkName: park.name,
+  // handleDelete = (park) => {
+  //   // const deleteUser = {
+  //   //   parkName: park.name,
 
-    // };
-    // console.log(park)
-    this.deleteUsers(park.name);
-  };
+  //   // };
+  //   // console.log(park)
+  //   this.deleteUsers(park.name);
+  // };
 
   componentDidMount() {
     this.pullUsers();
@@ -56,24 +56,47 @@ class NationalPark extends React.Component {
       .then((res) => res.__raw)
       .catch((err) => console.error(err));
   };
-  
+
   pullUsers = () => {
     this.getJwt()
-      .then(jwt => {
+      .then((jwt) => {
         const config = {
-          headers: { 'Authorization': `Bearer ${jwt}` }
-        }
+          headers: { Authorization: `Bearer ${jwt}` },
+        };
         const url = `${process.env.REACT_APP_SERVER}/users`;
         // console.log('The whole url getting from MongoDB- ', url)
         return axios.get(url, config);
       })
-      .then(response => {
+      .then((response) => {
         console.log(response.data); // Log the response data
         this.setState({ parks: response.data });
       })
-      .catch(err => console.error(err));
-      
-  }
+      .catch((err) => console.error(err));
+  };
+
+  deleteUsers = async (parkToDelete) => {
+    console.log("inside the delete function");
+    console.log(parkToDelete);
+    const url = `${process.env.REACT_APP_SERVER}/users/${parkToDelete._id}`;
+    this.getJwt()
+      .then((jwt) => {
+        const config = {
+          headers: { Authorization: `Bearer ${jwt}` },
+        };
+        const updatedUsers = axios.delete(url, config);
+        return updatedUsers;
+      })
+      .then((updatedUsers) => {
+        console.log(this.state.parks);
+        const updatedUsersArr = this.state.parks.filter(
+          (element) => element._id !== parkToDelete._id
+        );
+        this.setState({ parks: updatedUsersArr });
+      })
+      .catch((err) => console.error(err));
+    console.log(this.updatedUsers);
+  };
+
 
   // deleteUsers = async (parkToDelete) => {
   //   console.log("inside the delete function");
@@ -107,84 +130,95 @@ class NationalPark extends React.Component {
           newUser,
           config
         );
-
       })
-      .then(response => this.setState({ parks: [...this.state.parks, response.data] },() => console.log('checking whats in the parks state', this.state.parks)))
+      .then((response) =>
+        this.setState({ parks: [...this.state.parks, response.data] }, () =>
+          console.log("checking whats in the parks state", this.state.parks)
+        )
+      )
       .catch((err) => console.error(err));
   }
-  
+
   render() {
     return (
       <>
-      <div>
-        <p className="selectedCity">
-          This is your selected city: {this.props.city}
-        </p>
+        <div>
+          <p className="selectedCity">
+            This is your selected city: {this.props.city}
+          </p>
 
-        {this.props.displayInfo && (
-          <Accordion className="allInfo" defaultActiveKey="0">
-            {this.props.thisIsArrOfNationalPark.map((elements, idx) => (
-              <Accordion.Item eventKey={idx} key={idx}>
-                <Accordion.Header>{elements.name}</Accordion.Header>
+          {this.props.displayInfo && (
+            <Accordion className="allInfo" defaultActiveKey="0">
+              {this.props.thisIsArrOfNationalPark.map((elements, idx) => (
+                <Accordion.Item eventKey={idx} key={idx}>
+                  <Accordion.Header>{elements.name}</Accordion.Header>
 
-                <Accordion.Body>
-                  <div>
-                    <p>{elements.description}</p>
-                    <a
-                      href={elements.directions}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Click Here to see the direction
-                    </a>
+                  <Accordion.Body>
+                    <div>
+                      <p>{elements.description}</p>
+                      <a
+                        href={elements.directions}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Click Here to see the direction
+                      </a>
 
-                    {/* <img
+                      {/* <img
                       className="national_park"
                       alt={elements.name}
                       src={elements.image}
                     /> */}
 
-                    <p className="selectedCity">Standard Hours:</p>
+                      <p className="selectedCity">Standard Hours:</p>
 
-                    <ul>
-                      {Object.entries(elements.workHours).map(
-                        ([day, hours]) => (
-                          <li key={day}>
-                            {day}: {hours}
-                          </li>
-                        )
+                      <ul>
+                        {Object.entries(elements.workHours).map(
+                          ([day, hours]) => (
+                            <li key={day}>
+                              {day}: {hours}
+                            </li>
+                          )
+                        )}
+                      </ul>
+                      <p className="selectedCity">Yelp Reviews:</p>
+                      {this.props.yelpData.length > 0 &&
+                        this.renderYelpData(this.props.yelpData[idx])}
+                      <Button
+                        onClick={() => this.handleFavorite(elements, idx)}
+                      >
+                        Save to Favorite
+                      </Button>
+
+                      {this.state.isParkAdded === idx && (
+                        <h3>{elements.name} has been added to favorites!</h3>
                       )}
-                    </ul>
-                    <p className="selectedCity">Yelp Reviews:</p>
-                    {this.props.yelpData.length > 0 &&
-                      this.renderYelpData(this.props.yelpData[idx])}
-                    <Button onClick={() => this.handleFavorite(elements, idx)}>
-                      Save to Favorite
-                    </Button>
-                    
-                    {this.state.isParkAdded === idx && (
-                      <h3>{elements.name} has been added to favorites!</h3>
-                    )}
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))}
-          </Accordion>
-        )}
-        {this.props.errorIn && <p>Error: City not found.</p>}
-
-  
-      </div>
-      <div>
-
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          )}
+          {this.props.errorIn && <p>Error: City not found.</p>}
+        </div>
+        <div>
         <h4>The Favorite parks of your choice</h4>
-        <ol>
-          <li>
-            {this.state.parks}
-          </li>
-        </ol>
-      </div>
-         </>             
+      <Accordion>
+        {Object.keys(this.state.parks).map((key) => (
+          <Accordion.Item key={key}>
+            <Accordion.Header>{this.state.parks[key].parkName}</Accordion.Header>
+            <Accordion.Body>
+              <ol>
+              <Button onClick={() => this.deleteUsers(this.state.parks[key]._id)}>
+              Delete from the favorite
+            </Button>
+              </ol>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+        </div>
+      </>
     );
   }
 }
